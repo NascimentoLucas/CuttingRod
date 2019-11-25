0adad2c31826f0ef76d1fa60c22bb1eaa9f60b52
NascimentoLucas <nascimentol@outlook.com>
2019-11-25 19:12:22 -0300

    implemented data export

---
+++ CutRod.py
@@ -0,0 +1,34 @@
+import sys
+import time
+import random
+from Methods.TopDown import cut_rod as top_down_cut
+from Methods.BottomUp import cut_rod as bottom_up_cut
+
+
+def cut_rod(times, data):
+    data.time_top_down = 0
+    data.time_bottom_up = 0
+
+    if data.size < 100:
+        sys.setrecursionlimit(100)
+    else:
+        sys.setrecursionlimit(data.size * 2)
+
+    for i in range(times):
+        arr = []
+
+        for j in range(data.size):
+            arr.append(random.randint(1, 100))
+
+        start1 = time.time()
+        top_down_cut(arr, data.size)
+        end1 = time.time()
+        data.time_top_down += end1 - start1
+
+        start2 = time.time()
+        bottom_up_cut(arr, data.size)
+        end2 = time.time()
+        data.time_bottom_up += end2 - start2
+
+    data.time_top_down /= times
+    data.time_bottom_up /= times
--- Main.py
+++ Main.py
@@ -1,45 +1,32 @@
-import sys
-import time
-import random
-from TopDown import cut_rod as top_down_cut
-from BottomUp import cut_rod as bottom_up_cut
+from CutRod import cut_rod
 
 
-time_top_down = 0
-time_bottom_up = 0
-result_top_down = 0
-result_bottom_up = 0
+class Data:
+    def __init__(self):
+        self.size = 0
+        self.time_top_down = 0
+        self.time_bottom_up = 0
 
-r = 10
-size = 10
-sys.setrecursionlimit(size * 2)
-for i in range(r):
-    arr = []
+    def data_print(self):
+        s = str(self.size)
+        s += ';' + str(self.time_top_down)
+        s += ';' + str(self.time_bottom_up)
+        print(s)
 
-    for j in range(size):
-        arr.append(random.randint(1, 100))
+    @staticmethod
+    def data_print_model():
+        s = 'size'
+        s += ';time_top_down'
+        s += ';time_bottom_up'
+        print(s)
 
-    start1 = time.time()
-    result_top_down = top_down_cut(arr, size)
-    end1 = time.time()
-    time_top_down += end1 - start1
 
-    start2 = time.time()
-    result_bottom_up = bottom_up_cut(arr, size)
-    end2 = time.time()
-    time_bottom_up += end2 - start2
+d = Data()
+d.data_print_model()
+ran = 1000000
+time = 100
+for i in range(ran):
+    d.size = i + 1
+    cut_rod(time, d)
+    d.data_print()
 
-time_top_down /= r
-time_bottom_up /= r
-
-print('TopDown : BottomUp')
-print('Result: ')
-print(str(result_top_down) + ' : ' + str(result_bottom_up))
-print('Time: ')
-print(str(time_top_down) + ' : ' + str(time_bottom_up))
-
-if time_bottom_up > 0:
-    average = time_top_down / time_bottom_up
-else:
-    average = 0
-print('(time_top_down / time_bottom_up) => ' + str(average))

1d444385e640d6261fc1dcfe139b6c400e2be277
NascimentoLucas <nascimentol@outlook.com>
2019-11-25 21:04:20 -0300

    fixed wrong change between classes code

--- Methods/BottomUp.py
+++ Methods/BottomUp.py
@@ -1,14 +1,12 @@
 def cut_rod(price, n):
-    if n <= 0:
-        return 0
-    max_val = -1
+    val = [0 for x in range(n+1)]
+    val[0] = 0
 
-    val = 0
-    for i in range(0, n):
-        val = price[i] + cut_rod(price, n - i - 1)
-        if max_val < val:
-            max_val = val
-    return max_val
+    for i in range(1, n+1):
+        max_val = -1
+        for j in range(i):
+            max_val = max(max_val, price[j] + val[i-j-1])
+        val[i] = max_val
 
-
+    return val[n]
 
--- Methods/TopDown.py
+++ Methods/TopDown.py
@@ -1,12 +1,14 @@
 def cut_rod(price, n):
-    val = [0 for x in range(n+1)]
-    val[0] = 0
+    if n <= 0:
+        return 0
+    max_val = -1
 
-    for i in range(1, n+1):
-        max_val = -1
-        for j in range(i):
-            max_val = max(max_val, price[j] + val[i-j-1])
-        val[i] = max_val
+    val = 0
+    for i in range(0, n):
+        val = price[i] + cut_rod(price, n - i - 1)
+        if max_val < val:
+            max_val = val
+    return max_val
 
-    return val[n]
+
 
